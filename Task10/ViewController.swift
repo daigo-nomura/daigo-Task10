2455ef0ff73213fbb45371cf1e9756ec4198a498
daigo <daigon.0114@gmail.com>
2021-05-02 20:40:46 +0900

    Initial Commit

---
+++ Task10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task10
+//
+//  Created by 野村大悟 on 2021/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
