be3b0c07c74b54b45a0f19efab4309eab53b20e9
akio0911 <akio0911@gmail.com>
2021-06-23 18:16:14 +0900

    feedback

--- Task10/PrefecturalName.swift
+++ Task10/PrefecturalName.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-struct Prefectural {
-    static let prefecturalName = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
+struct Prefecture {
+    static let names = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
     "山形県", "福島県", "茨城県", "栃木県", "群馬県",
     "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
     "富山県", "石川県", "福井県", "山梨県", "長野県",
